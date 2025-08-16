f7ff0633e570d65c498745839c45eefd830ef178
c4id3nSecurity <leviizinho765@gmail.com>
2025-08-16 10:30:32 -0300

    feat: adicionar o Service de estatisticas

---
+++ src/main/java/com/rlevi/desafio_itau/service/EstatisticasService.java
@@ -0,0 +1,34 @@
+package com.rlevi.desafio_itau.service;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rlevi.desafio_itau.dto.EstatisticaDTO;
+import com.rlevi.desafio_itau.dto.TransacaoDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EstatisticasService {
+
+    private final TransacaoService transacaoService;
+
+    public EstatisticaDTO calcularEstatisticas(Integer intervalo) {
+    List<TransacaoDTO> transacao = transacaoService.buscarTransacao(intervalo);
+
+    DoubleSummaryStatistics estatisticasTransacao = transacao.stream().mapToDouble(TransacaoDTO::valor).summaryStatistics();
+    
+    EstatisticaDTO estatisticaDTO = new EstatisticaDTO(
+        estatisticasTransacao.getCount(), 
+        estatisticasTransacao.getSum(), 
+        estatisticasTransacao.getAverage(), 
+        estatisticasTransacao.getMax(), 
+        estatisticasTransacao.getMin()
+    );
+    
+    return estatisticaDTO;
+}
+}

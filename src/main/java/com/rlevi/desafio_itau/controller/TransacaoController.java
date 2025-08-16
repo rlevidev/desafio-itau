2873d2ff3da6a0c578c8aa18a47a80059cc71011
c4id3nSecurity <leviizinho765@gmail.com>
2025-08-16 10:50:46 -0300

    feat: adicionar o Controller de transação

---
+++ src/main/java/com/rlevi/desafio_itau/controller/TransacaoController.java
@@ -0,0 +1,27 @@
+package com.rlevi.desafio_itau.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rlevi.desafio_itau.dto.TransacaoDTO;
+import com.rlevi.desafio_itau.service.TransacaoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/transacao")
+@RequiredArgsConstructor
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    @PostMapping()
+    public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoDTO transacao) {
+        transacaoService.adicionarTransacao(transacao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
